7ee2957562c78ab0a4e90c5fa3be3d2bb954ec27
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2021-12-16 09:55:46 -0500

    added musiccc

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.mp3$/,
+        use: {
+          loader: 'url-loader',
+        },
+      });
+      return config;
+    },
+  }
