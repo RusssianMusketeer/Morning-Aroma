dcde0fecf67712e2e615fd077d1382fe34d0b128
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2021-12-15 22:19:02 -0500

    added music

--- src/Components/NavBar/NavBar.component.jsx
+++ src/Components/NavBar/NavBar.component.jsx
@@ -6,15 +6,21 @@ import {ReactComponent as Cart} from "../../Assets/Cart.svg";
 import {ReactComponent as Logo} from "../../Assets/Morning Aroma.svg";
 import useSound from 'use-sound';
 import music from "../../Assets/Jazz.mp3";
+import Toggle from 'react-toggle'
 
 const NavBar = () => {
     const [scroll, setScroll] = useState(false);
-    const [play] = useSound(music);
+    const [checked, setChecked] = useState(true);
+    const [play,{stop}] = useSound(music, {interrupt: true});
+    console.log(checked)
     useEffect(() => {
         window.addEventListener("scroll", () => {
           setScroll(window.scrollY > 60);
         });
-      }, []); 
+      }, []);
+      useEffect(() => {
+        play();
+      }, [play]);  
     return (
         <nav className="HomePage-NavBar" style={scroll ? {backgroundColor:"#282828", padding: "7px 30px"} : {backgroundColor:"transparent"} }>
             <div className="NavBar-Container">
@@ -42,6 +48,15 @@ const NavBar = () => {
                             <Link className={scroll ? "Navbar-Links" : "Navbar-Links-Scroll"} to="/sign-in">
                                 SIGN IN
                             </Link>
+                        </li>
+
+                        <li>
+                        <Toggle
+                        defaultChecked={checked}
+                        checked={checked===true ? true : false}
+                        onChange={checked=== true ? ()=>{setChecked(!checked); stop()} :()=>{setChecked(!checked); play() }}
+                        />
+
                         </li>
                         <li>
                         <Link to="/cart" >
