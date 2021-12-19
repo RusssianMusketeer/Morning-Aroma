3069e3a96075047b301508b1d3d465d8e2958933
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2021-12-18 19:32:07 -0500

    added toggle animation for all pages

--- src/Context/musicContext.js
+++ src/Context/musicContext.js
@@ -3,11 +3,17 @@ import useSound from "use-sound";
 import music from "../Assets/Jazz.mp3";
 import { useState } from "react";
 
+
 const MusicContext= createContext();
 
 export const MusicProvider =({children}) => {
-    const [play,{stop}] = useSound(music, {interrupt: true});
     const [checked, setChecked] = useState(false);
+    const [play,{stop}] = useSound(music, {
+        loop: true,
+        interrupt: true,
+        volume: 0.7,
+    });
+    
 
     return (
         <MusicContext.Provider value={{play,stop,checked,setChecked}}>
